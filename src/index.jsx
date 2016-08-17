3ef7d5907616480b94fc6c73b2527295201914a8
PAK90 <paklavins15@hotmail.com>
2016-08-16 21:12:53 -0600

    Header experiment

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-require("../node_modules/bootstrap/dist/css/bootstrap.min.css");
+//require("../node_modules/bootstrap/dist/css/bootstrap.min.css");
 import React from 'react';
 import ReactDOM from 'react-dom';
 require("./styles/customisations.scss");
@@ -6,18 +6,17 @@ require("./styles/customisations.scss");
 export class App extends React.Component {
 	render() {
 		return (
+				<div className="body">
 
-			<div className="container">
-				<div className="jumbotron">
-
+					<div className="header">
+						<h3>WhatIsCooler.com</h3>
+					</div>
 					<div>
 						<h1>Jumbotron</h1>
 						<p>This is a simple hero unit, a jumbotron-style component for calling extra attention to featured content or information.</p>
 						<p><a className="btn btn-primary btn-lg">Learn more</a></p>
-					</div>
-					
+					</div>					
 				</div>
-			</div>
 		);
 	}
 }
