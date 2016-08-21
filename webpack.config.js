8ce35bac9e53b1c2d52f8fbeebf78e124c1d7875
Pēteris Kļaviņš <klavins@netspace.net.au>
2016-08-22 00:42:20 +0100

    Add production build capability to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,54 @@
 "use strict";
-var webpack = require('webpack');
-var path = require('path');
-var loaders = require('./webpack.loaders');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const loaders = require('./webpack.loaders');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || "3333";
 
-module.exports = {
-	entry: [
-		`webpack-dev-server/client?http://${HOST}:${PORT}`, // WebpackDevServer host and port
-		`webpack/hot/only-dev-server`,
-		`./src/index.js` // Your appʼs entry point
-	],
-	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	module: {
-		loaders
-	},
-	devServer: {
-		contentBase: "./public",
-			noInfo: true, //  --no-info option
-			hot: true,
-			inline: true,
-			port: PORT,
-			host: HOST
-		},
-	plugins: [
-		new webpack.NoErrorsPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-		new CopyWebpackPlugin([
-			{from: './index.html'}
-		]),
-	]
-};
+const config = {}
+
+config.entry = []
+
+if (process.env.NODE_ENV === 'development') {
+  config.devtool = process.env.WEBPACK_DEVTOOL || 'source-map';
+  config.entry.push('webpack-dev-server/client?http://${HOST}:${PORT}'); // WebpackDevServer host and port
+  config.entry.push('webpack/hot/only-dev-server');
+}
+
+config.entry.push(path.join(__dirname, './src/index.js')); // Your app's entry point
+
+config.output = {
+  path: path.join(__dirname, 'public'),
+  filename: 'bundle.js'
+}
+
+config.resolve = {
+  extensions: ['', '.js', '.jsx']
+}
+
+config.module = {
+  loaders
+}
+
+config.devServer = {
+  contentBase: "./public",
+  noInfo: true, //  --no-info option
+  hot: true,
+  inline: true,
+  port: PORT,
+  host: HOST
+}
+
+config.plugins = [
+  new webpack.NoErrorsPlugin(),
+  new webpack.HotModuleReplacementPlugin(),
+  new CopyWebpackPlugin([
+    { from: './index.html' },
+    { from: './favicon-16x16.png' },
+    { from: './favicon-32x32.png' }
+  ])
+]
+
+module.exports = config;
